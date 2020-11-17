70567c7704d56508a43b80385e21972622c2c706
4i4a <cleptic3@gmail.com>
2020-11-17 12:56:43 +0200

    Added var's

--- JSBlank.js
+++ JSBlank.js
@@ -1,3 +1,17 @@
-"use strict";
-var input1 = document.querySelector(".input");
-console(input1.value);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 1);
+let personalMovieDB = {
+    count : numberOfFilms   ,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+let LastFilm = prompt("Один из последних просмотренных фильмов?" ,
+ "Пример : Шарик"),
+ Rate = prompt("На сколько оцените его?", "1-10") ,
+ LastFilm2 = prompt("Один из последних просмотренных фильмов?" ,
+ "Пример : Шарик"),
+ Rate2 = prompt("На сколько оцените его?", "1-10") ;
+personalMovieDB.movies[LastFilm] = Rate;
+personalMovieDB.movies[LastFilm2] = Rate2;
+console.log(personalMovieDB.movies);
